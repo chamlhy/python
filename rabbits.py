ddedb27080e55116d628925c5e91393ed3842351
chamlhy <lhydreams@163.com>
2017-04-15 14:37:41 +0800

    rabit problem/Fibonacci

---
+++ rabbits.py
@@ -0,0 +1,35 @@
+#iterate
+def feb(n):
+    n1 = 1
+    n2 = 1
+    n3 = 1
+    if n < 1:
+        print('input error!')
+        return -1
+    while (n-2) > 0:
+        n3 = n2 + n1
+        n1 = n2
+        n2 = n3
+        n -= 1
+    return n3
+
+# recurrence   
+def feb1(n):
+    if n < 1:
+        print('input error!')
+        return -1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    if n > 2:
+        return feb1(n-1)+feb1(n-2)
+
+if __name__ == '__main__':
+    result = feb(20)
+    result1 = feb1(20)
+    if result == result1:
+        print("After 20 months, rabbit's number will be %d. " % result)
+    else:
+        print("computation error!")
+        
